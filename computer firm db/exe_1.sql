d34550a824832850daab5c2017b69b04758355be
pacificus93 <49984827+pacificus93@users.noreply.github.com>
2023-04-23 17:09:04 -0400

    Create exe_1.sql

---
+++ computer firm db/exe_1.sql
@@ -0,0 +1,10 @@
+/*
+Find the model number, speed and hard drive capacity for all the PCs with prices below $500.
+Result set: model, speed, hd.
+*/
+
+SELECT model
+      ,speed
+      ,hd 
+FROM pc 
+WHERE price < 500
