14546479a44816a4da16d9062e9c8c3e57ba0f79
pacificus93 <49984827+pacificus93@users.noreply.github.com>
2023-04-22 23:29:28 -0400

    Create exe_20.sql

---
+++ computer firm db/exe_20.sql
@@ -0,0 +1,12 @@
+/*
+Find the makers producing at least three distinct models of PCs.
+Result set: maker, number of PC models.
+*/
+
+SELECT maker
+      ,COUNT(DISTINCT model)
+FROM product AS pd
+WHERE type = 'PC'
+GROUP BY maker
+HAVING COUNT(DISTINCT model) > 2
+
