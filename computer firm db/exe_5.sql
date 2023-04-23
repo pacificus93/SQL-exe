343221263a75080002541da8d98fe5bbd0ff18c5
pacificus93 <49984827+pacificus93@users.noreply.github.com>
2023-04-23 17:09:33 -0400

    Create exe_5.sql

---
+++ computer firm db/exe_5.sql
@@ -0,0 +1,10 @@
+/*
+Find the model number, speed and hard drive capacity of PCs 
+cheaper than $600 having a 12x or a 24x CD drive.
+*/
+
+SELECT model
+      ,speed
+      ,hd 
+FROM pc 
+WHERE price < 600 AND cd IN ('12x','24x')
