d38c24969cb86acd6e49045ba7a9e434a12166eb
German Zakharov <german.zakharov12@gmail.com>
2025-07-11 20:19:30 -0400

    Update exe_68.sql

--- airport db/exe_68.sql
+++ airport db/exe_68.sql
@@ -1,17 +1,20 @@
-WITH summary AS (
+WITH route_summary AS (
+	--Count the number of trips for each Route
 	SELECT 
 	   town_from,
 	   town_to,
 	   COUNT(trip_no) AS cnt
 	FROM(
+		--"Equating" Routes A - B and B - A
 		SELECT 
 		   CASE WHEN town_from < town_to THEN town_from ELSE town_to END AS town_from,
 		   CASE WHEN town_from < town_to THEN town_to ELSE town_from END AS town_to,
 		   trip_no
 		FROM trip) AS s 
 	GROUP BY town_from,town_to)
+--Count the number of routes with the maximum number of trips
 SELECT COUNT(*) 
-FROM summary 
+FROM route_summary 
 WHERE cnt = (
 	SELECT MAX(cnt) 
-	FROM summary)
+	FROM route_summary)
